f5cc3af8356985c4b9d9c79e9f51f0322e58b669
Pavithragpi <pavithragopi99@gmail.com>
2026-04-26 20:34:26 -0500

    Update dxf_reader.py

--- dxf_reader.py
+++ dxf_reader.py
@@ -1,1 +1,20 @@
+import ezdxf
 
+def read_dxf(file):
+    doc = ezdxf.readfile(file)
+    return doc.modelspace()
+
+def extract_columns(msp):
+    columns = []
+
+    for entity in msp:
+        if entity.dxftype() == "INSERT":
+            point = entity.dxf.insert
+
+            columns.append({
+                "name": entity.dxf.name,
+                "x": round(point.x, 2),
+                "y": round(point.y, 2)
+            })
+
+    return columns
