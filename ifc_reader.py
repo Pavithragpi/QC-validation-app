f96a76cf7076cd9ccdca9019bd7b4a6735198dc2
Pavithragpi <pavithragopi99@gmail.com>
2026-04-26 20:34:54 -0500

    Update ifc_reader.py

--- ifc_reader.py
+++ ifc_reader.py
@@ -1,1 +1,22 @@
+import ifcopenshell
 
+def load_ifc(file):
+    return ifcopenshell.open(file)
+
+def get_ifc_columns(model):
+    columns = []
+
+    for col in model.by_type("IfcColumn"):
+        try:
+            loc = col.ObjectPlacement.RelativePlacement.Location.Coordinates
+
+            columns.append({
+                "id": col.GlobalId,
+                "x": round(loc[0], 2),
+                "y": round(loc[1], 2),
+                "z": round(loc[2], 2)
+            })
+        except:
+            continue
+
+    return columns
