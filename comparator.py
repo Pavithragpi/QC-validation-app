53c872679e7f058d27295829e9601fd7d3f80141
Pavithragpi <pavithragopi99@gmail.com>
2026-04-26 20:35:23 -0500

    Update comparator.py

--- comparator.py
+++ comparator.py
@@ -1,1 +1,15 @@
+def compare(structural, erection, tol=1.0):
+    missing = []
 
+    for s in structural:
+        found = False
+
+        for e in erection:
+            if abs(s["x"] - e["x"]) < tol and abs(s["y"] - e["y"]) < tol:
+                found = True
+                break
+
+        if not found:
+            missing.append(s)
+
+    return missing
